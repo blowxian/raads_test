3fbf877370824597a94c02c316b572ee448a8d31
lisonallen <lisonallen@qq.com>
2024-08-06 02:08:08 +0800

    bugfix;

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,6 @@
 import type {Metadata} from "next";
-import {Inter} from "next/font/google";
 import "@/app/globals.css";
 import Script from "next/script";
-
-const inter = Inter({subsets: ["latin"]});
 
 export const metadata: Metadata = {
     title: "RAADS-R Assessment Report | Autism Evaluation Results",
